d69736a89288eb65ac41cf5317f4344a34e09e50
Mark Wragg <m.wragg@reply.eu>
2024-01-09 17:45:19 +0000

    Added basic module test

---
+++ Tests/AzCostTools.Tests.ps1
@@ -0,0 +1,16 @@
+﻿Describe "AzCostTools Module Tests PS$PSVersion" {
+
+    BeforeAll {
+        if (-not $PSScriptRoot) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+
+        $PSVersion = $PSVersionTable.PSVersion.Major
+        $Root = "$PSScriptRoot\.."
+        $Module = 'AzCostTools'
+
+        Get-Module $Module | Remove-Module -Force
+    }
+    
+    It "Should import $Module without errors" {
+        { Import-Module (Join-Path $Root $Module) -Force -ErrorAction Stop } | Should -Not -Throw
+    }
+}
