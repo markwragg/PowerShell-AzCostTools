acb042f1aa78862ffe65ac36f66c26243c340415
Mark Wragg <m.wragg@reply.eu>
2024-01-13 10:10:28 +0000

    Test coverage improvements

--- Tests/Show-CostAnalysis.tests.ps1
+++ Tests/Show-CostAnalysis.tests.ps1
@@ -5,48 +5,87 @@ Describe Show-CostAnalysis {
     InModuleScope AzCostTools {
 
         BeforeAll {
-            $Cost = [PSCustomObject]@{
-                Name                       = 'SomeSubscription'
-                BillingPeriod              = '202401'
-                Currency                   = 'EUR'
-                Cost                       = '500'
-                DailyCost_SparkLine        = '▃▇▇▇▇▃▄▇███▁'
-                DailyCost_Min              = '18'
-                DailyCost_Max              = '50'
-                DailyCost_Avg              = '40'
-                MostExpensive_Date         = (Get-Date '09/01/2024 00:00:00')
-                LeastExpensive_Date        = (Get-Date '12/01/2024 00:00:00')
-                DailyCost                  = @(
-                    @{
-                        Date = (Get-Date '01/01/2024 00:00:00')
-                        Cost = 100
-                    },
-                    @{
-                        Date = (Get-Date '02/01/2024 00:00:00')
-                        Cost = 200
-                    },
-                    @{
-                        Date = (Get-Date '03/01/2024 00:00:00')
-                        Cost = 200
-                    }
-                )
-                CostPerService             = @( 
-                    @{
-                        Service = 'Microsoft.Automation'
-                        Cost    = 0.007885 
-                    } 
-                )
-                MostExpensiveService       = 'Microsoft.Compute'
-                MostExpensiveService_Cost  = 250
-                LeastExpensiveService      = 'microsoft.insights'
-                LeastExpensiveService_Cost = 0
-                ActiveBudgets              = @(
-                    @{
-                        BudgetAmount    = 1000
-                        BudgetTimeGrain = 'Monthly'
-                    }
-                )
-            }
+            $Cost = @(
+                [PSCustomObject]@{
+                    Name                       = 'SomeSubscription'
+                    BillingPeriod              = '202401'
+                    Currency                   = 'EUR'
+                    Cost                       = '500'
+                    DailyCost_SparkLine        = '▃▇▇▇▇▃▄▇███▁'
+                    DailyCost_Min              = '18'
+                    DailyCost_Max              = '50'
+                    DailyCost_Avg              = '40'
+                    MostExpensive_Date         = (Get-Date '09/01/2024 00:00:00')
+                    LeastExpensive_Date        = (Get-Date '12/01/2024 00:00:00')
+                    DailyCost                  = @(
+                        @{
+                            Date = (Get-Date '01/01/2024 00:00:00')
+                            Cost = 100
+                        },
+                        @{
+                            Date = (Get-Date '02/01/2024 00:00:00')
+                            Cost = 200
+                        },
+                        @{
+                            Date = (Get-Date '03/01/2024 00:00:00')
+                            Cost = 200
+                        }
+                    )
+                    CostPerService             = @( 
+                        @{
+                            Service = 'Microsoft.Network'
+                            Cost    = 100 
+                        } 
+                    )
+                    MostExpensiveService       = 'Microsoft.Compute'
+                    MostExpensiveService_Cost  = 250
+                    LeastExpensiveService      = 'microsoft.insights'
+                    LeastExpensiveService_Cost = 0
+                    ActiveBudgets              = @(
+                        @{
+                            BudgetAmount    = 400
+                            BudgetTimeGrain = 'Monthly'
+                        }
+                    )
+                }
+                [PSCustomObject]@{
+                    Name                       = 'SomeOtherSubscription'
+                    BillingPeriod              = '202401'
+                    Currency                   = 'EUR'
+                    Cost                       = '5000'
+                    DailyCost_SparkLine        = '▃▇▇▇▇▃▄▇███▁'
+                    DailyCost_Min              = '18'
+                    DailyCost_Max              = '50'
+                    DailyCost_Avg              = '40'
+                    MostExpensive_Date         = (Get-Date '09/01/2024 00:00:00')
+                    LeastExpensive_Date        = (Get-Date '12/01/2024 00:00:00')
+                    DailyCost                  = @(
+                        @{
+                            Date = (Get-Date '01/01/2024 00:00:00')
+                            Cost = 10
+                        },
+                        @{
+                            Date = (Get-Date '02/01/2024 00:00:00')
+                            Cost = 20
+                        },
+                        @{
+                            Date = (Get-Date '03/01/2024 00:00:00')
+                            Cost = 30
+                        }
+                    )
+                    CostPerService             = @( 
+                        @{
+                            Service = 'Microsoft.Compute'
+                            Cost    = 10 
+                        } 
+                    )
+                    MostExpensiveService       = 'Microsoft.Compute'
+                    MostExpensiveService_Cost  = 250
+                    LeastExpensiveService      = 'microsoft.insights'
+                    LeastExpensiveService_Cost = 0
+                    ActiveBudgets              = @()
+                }
+            )
 
             Mock Write-Host {}
 
@@ -55,7 +94,32 @@ Describe Show-CostAnalysis {
             function Show-Sparkline {}
 
             Mock New-Emphasis
-            Mock Get-Sparkline
+            
+            Mock Get-Sparkline { 
+                @{
+                    Row   = 0
+                    Col   = 0
+                    Val   = 1
+                    Block = '▁'
+                    Color = 'Gray'
+                }
+                @{
+                    Row   = 0
+                    Col   = 1
+                    Val   = 4
+                    Block = '▄'
+                    Color = 'Gray'
+                }
+                @{
+                
+                    Row   = 0
+                    Col   = 2
+                    Val   = 8
+                    Block = '█'
+                    Color = 'Gray'
+                }
+            }
+
             Mock Show-SparkLine
         }
 
@@ -66,8 +130,17 @@ Describe Show-CostAnalysis {
             }
 
             $Cost | Show-CostAnalysis
-            Should -Invoke Write-Host -Times 20 -Exactly
+            Should -Invoke Write-Host -Times 61 -Exactly
         }
 
+        It 'Should return cost analysis when the input is sent via the pipeline and draw Sparklines' {
+            
+            Mock Test-PSparklinesModule {
+                $true
+            }
+    
+            $Cost | Show-CostAnalysis
+            Should -Invoke Write-Host -Times 69 -Exactly
+        }
     }
 }
