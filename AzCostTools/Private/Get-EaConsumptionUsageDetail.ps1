6fa5d1f7594aa451d648dc97d1a542a308e1f3c8
Mark Wragg <m.wragg@reply.eu>
2024-09-10 17:19:04 +0100

    Fix for Enterprise Agreement costs not in billing currency

--- AzCostTools/Private/Get-EaConsumptionUsageDetail.ps1
+++ AzCostTools/Private/Get-EaConsumptionUsageDetail.ps1
@@ -10,7 +10,7 @@ function Get-EaConsumptionUsageDetail {
     param (
         [string]
         $SubscriptionId = (Get-AzContext).Subscription.Id,
-  
+
         [ValidateSet('Legacy', 'Modern')]
         [string]
         $SubscriptionKind = 'Modern',
@@ -19,23 +19,23 @@ function Get-EaConsumptionUsageDetail {
         [string]
         $BillingPeriodName
     )
-  
+
     $StartDate = [datetime]::parseexact($BillingPeriodName, 'yyyyMM', $null)
     $EndDate = $StartDate.AddMonths(1).AddDays(-1)
-    
+
     $isLegacy = $SubscriptionKind -eq 'Legacy'
-  
+
     $resource = 'https://management.azure.com'
     $context = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile.DefaultContext
     $accessToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, $resource).AccessToken
 
-    
+
     $Date = $StartDate.AddDays($Day)
-    
+
     $dateFilter = $Date.Date.ToString('yyyy-MM-dd')
     $endDateFilter = $EndDate.Date.ToString('yyyy-MM-dd')
     $uriPath = "https://management.azure.com/subscriptions/$($SubscriptionId)/providers/Microsoft.Consumption/usageDetails"
-  
+
     # https://docs.microsoft.com/en-us/azure/cost-management-billing/costs/manage-automation#get-usage-details-for-a-scope-during-specific-date-range
     if ($isLegacy) {
         $uriQuery = '?$expand=properties/meterDetails&$filter=properties/usageStart ge ''' + $dateFilter + ''' and properties/usageEnd le ''' + $endDateFilter + '''&$top=1000&api-version=2019-10-01'
@@ -61,7 +61,7 @@ function Get-EaConsumptionUsageDetail {
         $consumptionDetailsRaw
 
     } until (-not $nextLink)
-    
+
     $consumptionDetails = @()
 
     foreach ($detail in $consumptionRaw.value) {
@@ -86,11 +86,11 @@ function Get-EaConsumptionUsageDetail {
                 MeterLocation    = $detail.properties.resourceLocation
             }
             UsageQuantity     = $detail.properties.quantity
-            PreTaxCost        = if ($isLegacy) { $detail.properties.cost } else { $detail.properties.costInUSD }
+            PreTaxCost        = if ($isLegacy) { $detail.properties.cost } else { $detail.properties.costInBillingCurrency }
         }
-  
+
         $consumptionDetails += $convertedDetail
     }
-  
+
     $consumptionDetails
 }
